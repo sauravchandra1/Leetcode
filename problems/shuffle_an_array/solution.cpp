35400b22058a675f39ad4f2afdd5aa20c582a13c
Saurav Chandra <sauravchandra125@gmail.com>
2020-08-19 09:30:50 +0000

    Sync LeetCode submission - Shuffle an Array (cpp)

---
+++ problems/shuffle_an_array/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    public:
+        vector < int > temp;
+    Solution(vector < int > & nums) {
+        temp.clear();
+        temp = nums;
+    }
+
+    /** Resets the array to its original configuration and return it. */
+    vector < int > reset() {
+        vector < int > res;
+        res = temp;
+        return res;
+    }
+
+    /** Returns a random shuffling of the array. */
+    vector < int > shuffle() {
+        vector < int > res;
+        res = temp;
+        int n = temp.size();
+        for (int i = n - 1; i >= 1; --i) {
+            int j = rand() % (i + 1);
+            swap(res[i], res[j]);
+        }
+        return res;
+    }
+};
