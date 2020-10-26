ba4285cb353879b2d8ead8a533fad7c296a64705
Saurav Chandra <sauravchandra125@gmail.com>
2020-10-26 06:34:12 +0000

    Sync LeetCode submission - Combination Sum (cpp)

--- problems/combination_sum/solution.cpp
+++ problems/combination_sum/solution.cpp
@@ -2,25 +2,19 @@ class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
-        int n = candidates.size();
-        if (n == 0) {
-            return ans;
-        }
-        vector<int> res;
+        vector<int> vec;
         sort(candidates.begin(), candidates.end());
-        function<void(int, int)> dfs = [&](int idx, int target) {
-            if (target == 0) {
-                ans.push_back(res);
+        int n = candidates.size();
+        function<void(int, int)> dfs = [&](int idx, int sum) {
+            if (sum == 0) {
+                ans.push_back(vec);
                 return ;
             }
-            if (idx == n) return ;
             for (int i = idx; i < n; i++) {
-                if (candidates[i] > target) {
-                    break;
-                }
-                res.push_back(candidates[i]);
-                dfs(i, target - candidates[i]);
-                res.pop_back();
+                if (sum < candidates[i]) break;
+                vec.push_back(candidates[i]);
+                dfs(i, sum - candidates[i]);
+                vec.pop_back();
             }
         };
         dfs(0, target);
