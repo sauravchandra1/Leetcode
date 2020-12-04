ec6be6e59b0cf4982eb048410d2d67e384c7d688
Saurav Chandra <sauravchandra125@gmail.com>
2020-12-04 15:14:43 +0000

    Sync LeetCode submission - Longest Palindromic Substring (cpp)

--- problems/longest_palindromic_substring/solution.cpp
+++ problems/longest_palindromic_substring/solution.cpp
@@ -1,29 +1,52 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.size();
-        auto make = [&](int i, int j) {
-            tuple<int, int, int> res{1, 0, 0};
-            while (i >= 0 && j < n) {
-                if (s[i] == s[j]) {
-                    i--, j++;
-                } else break;
+        int len = s.size();
+        string ans;
+        if (len == 0) return ans;
+        if (len == 1) return s;
+        const int inf = 1e4;
+        int _max1 = -inf, _l1, _r1;
+        // Check if the longest substring have a center character
+        for (int i = 0; i < len; i++) {
+            int l = i - 1, r = i + 1;
+            while (l >= 0 && r < len && s[l] == s[r]) {
+                l--, r++;
             }
-            get<0>(res) = j - i - 1;
-            get<1>(res) = i + 1;
-            get<2>(res) = j - 1;
-            return res;
-        };
-        tuple<int, int, int> res{1, 0, 0};
-        for (int i = 1; i < n; i++) {
-            tuple a = make(i - 1, i);
-            tuple b = make(i - 1, i + 1);
-            if (get<0>(a) > get<0>(res) && get<0>(a) > get<0>(b)) {
-                res = a;
-            } else if (get<0>(b) > get<0>(res) && get<0>(b) > get<0>(a)) {
-                res = b;
+            l++, r--;
+            if ((r - l + 1) > _max1) {
+                _max1 = r - l + 1;
+                _l1 = l;
+                _r1 = r;
             }
         }
-        return s.substr(get<1>(res), get<0>(res));
+        int _max2 = -inf, _l2, _r2;
+        // Check if the longest substring have two same charaters in center
+        for (int i = 0; i < len; i++) {
+            if (i + 1 < len && s[i] == s[i + 1]) {
+                int l = i - 1, r = i + 2;
+                while (l >= 0 && r < len && s[l] == s[r]) {
+                    l--, r++;
+                }
+                l++, r--;
+                if ((r - l + 1) > _max2) {
+                    _max2 = r - l + 1;
+                    _l2 = l;
+                    _r2 = r;
+                }
+            }
+        }
+        int l, r;
+        if (_max1 > _max2) {
+            l = _l1;
+            r = _r1;
+        } else {
+            l = _l2;
+            r = _r2;
+        }
+        for (int i = l; i <= r; i++) {
+            ans += s[i];
+        }
+        return ans;
     }
 };
