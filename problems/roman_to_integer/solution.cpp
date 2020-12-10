401c277a92db323c4a669a129daa55a41d337b67
Saurav Chandra <sauravchandra125@gmail.com>
2020-12-10 05:22:22 +0000

    Sync LeetCode submission - Roman to Integer (cpp)

--- problems/roman_to_integer/solution.cpp
+++ problems/roman_to_integer/solution.cpp
@@ -1,55 +1,22 @@
 class Solution {
 public:
-     int romanToInt(string s) {
-     int sum = 0;
-     char c;
-     int i;
-     for (i = 0; i < s.size(); i++) {
-         if (s[i] == 'C') {
-             if (i != s.size() - 1) {
-                 if (s[i + 1] == 'D') {
-                     sum += 400;
-                     i++;
-                 } else if (s[i + 1] == 'M') {
-                     sum += 900;
-                     i++;
-                 } else
-                     sum += 100;
-             } else
-                 sum += 100;
-         } else if (s[i] == 'I') {
-             if (i != s.size() - 1) {
-                 if (s[i + 1] == 'V') {
-                     sum += 4;
-                     i++;
-                 } else if (s[i + 1] == 'X') {
-                     sum += 9;
-                     i++;
-                 } else
-                     sum += 1;
-             } else
-                 sum += 1;
-         } else if (s[i] == 'X') {
-             if (i != s.size() - 1) {
-                 if (s[i + 1] == 'L') {
-                     sum += 40;
-                     i++;
-                 } else if (s[i + 1] == 'C') {
-                     sum += 90;
-                     i++;
-                 } else
-                     sum += 10;
-             } else
-                 sum += 10;
-         } else if (s[i] == 'V') {
-             sum += 5;
-         } else if (s[i] == 'L')
-             sum += 50;
-         else if (s[i] == 'D')
-             sum += 500;
-         else
-             sum += 1000;
-     }
-     return sum;
- }
+    int romanToInt(string s) {
+        map<char, int> mp;
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
+        int ans = 0;
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (i + 1 < s.size() && mp[s[i]] < mp[s[i + 1]]) {
+                ans -= mp[s[i]];
+            } else {
+                ans += mp[s[i]];
+            }
+        }
+        return ans;
+    }
 };
