d7eb2cb88ee515cbec13101d7da050e21bf22d0b
Saurav Chandra <sauravchandra125@gmail.com>
2020-12-19 16:01:06 +0000

    Sync LeetCode submission - Unique Paths (cpp)

--- problems/unique_paths/solution.cpp
+++ problems/unique_paths/solution.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int> dx {0, 1};
-        vector<int> dy {1, 0};
-        auto go = [&](int x, int y) {
-            if (x < 0 || x >= m || y < 0 || y >= n) return false;
-            return true;
-        };
-        vector<vector<int>> dp(m, vector<int> (n, -1));
-        function<int(int, int)> dfs = [&](int x, int y) {
-            if (x == m - 1 && y == n - 1) return 1;
-            int &res = dp[x][y];
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1, -1));
+        function<int(int, int)> dfs = [&](int m, int n) {
+            if (m < 0 || n < 0) return 0;
+            if (m == 1 && n == 1) return 1;
+            auto &res = dp[m][n];
             if (res != -1) return res;
             res = 0;
-            int xx, yy;
-            for (int i = 0; i < 2; i++) {
-                xx = x + dx[i];
-                yy = y + dy[i];
-                if (go(xx, yy)) {
-                    res += dfs(xx, yy);
-                }
-            }
+            res = dfs(m - 1, n) + dfs(m, n - 1);
             return res;
         };
-        return dfs(0, 0);
+        return dfs(m, n);
     }
 };
