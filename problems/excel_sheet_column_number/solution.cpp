4438f4a1c576db5fb4405b954c499fc85cc87982
Saurav Chandra <sauravchandra125@gmail.com>
2020-12-19 15:26:25 +0000

    Sync LeetCode submission - Excel Sheet Column Number (cpp)

--- problems/excel_sheet_column_number/solution.cpp
+++ problems/excel_sheet_column_number/solution.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     int titleToNumber(string s) {
+        int idx = s.size() - 1;
         int ans = 0;
-        auto pwr = [&](long long x, long long y) {
-            long long res = 1;
-            while (y > 0) {
-                if (y & 1) {
-                    res = res * x;
-                }
-                y = y >> 1;
-                x = x * x;
-            }
-            return res;
-        };
-        for (int i = s.size() - 1; i >= 0; i--) {
-            ans += pwr(26, s.size() - i - 1) * (s[i] - 'A' + 1);
+        for (int i = 0; i < s.size(); i++) {
+            auto ch = s[s.size() - i - 1];
+            ans += pow(26, i) * (ch - 'A' + 1);
         }
         return ans;
     }
