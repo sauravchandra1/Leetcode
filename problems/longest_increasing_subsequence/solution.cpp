717e4da249cb6cec90211e4535b290d74d60235c
Saurav Chandra <sauravchandra125@gmail.com>
2021-03-04 10:51:30 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (cpp)

--- problems/longest_increasing_subsequence/solution.cpp
+++ problems/longest_increasing_subsequence/solution.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int len = nums.size();
-        vector<int> dp(len, -1);
-        function<int(int)> dfs = [&](int idx) {
-            if (idx == len) return 0;
-            if (idx == len - 1) return 1;
-            auto &res = dp[idx];
-            if (res != -1) return res;
-            res = 1;
-            for (int i = idx + 1; i < len; i++) {
-                if (nums[i] > nums[idx]) {
-                    res = max(res, dfs(i) + 1);
-                }
+        int n = nums.size();
+        if (n == 1) return n;
+        vector<int> dp(n + 1, -1);
+        function<int(int)> recur = [&](int idx) {
+            if (idx == n - 1) {
+                return 1;
             }
+            auto& res = dp[idx];
+            if (res != -1) return res;
+            res = 0;
+            for (int i = idx + 1; i < n; i++) {
+                if (nums[i] > nums[idx]) res = max(res, recur(i));
+            }
+            res++;
             return res;
         };
         int ans = 0;
-        for (int i = 0; i < len; i++) {
-            ans = max(ans, dfs(i));
-        }
+        for (int i = 0; i < n; i++) ans = max(ans, recur(i));
+        // for (int i = 0; i < n; i++) cout << dp[i] << " ";
         return ans;
     }
 };
