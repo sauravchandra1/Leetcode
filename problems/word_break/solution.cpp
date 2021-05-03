26ec7bad55ca2280a80443f99a6f094a97e24b36
Saurav Chandra <sauravchandra125@gmail.com>
2021-05-03 05:36:03 +0000

    Sync LeetCode submission - Word Break (cpp)

--- problems/word_break/solution.cpp
+++ problems/word_break/solution.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        int len = s.size();
-        unordered_set<string> ump;
-        for (auto v : wordDict) ump.insert(v);
-        vector<int> dp(len + 1, -1);
+        unordered_map<string, bool> mp;
+        int len = wordDict.size();
+        for (int i = 0; i < len; i++) mp[wordDict[i]] = true;
+        vector<int> dp(s.size() + 1, -1);
         function<int(int)> dfs = [&](int idx) {
-            if (idx == len) return 1;
-            auto& ans = dp[idx];
-            if (ans != -1) return ans;
-            ans = 0;
-            string str;
-            for (int i = idx; i < len; i++) {
-                str += s[i];
-                if (ump.find(str) != ump.end())
-                    ans = dfs(i + 1);
-                if (ans) break;
+            if (idx == s.size()) return 1;
+            auto& res = dp[idx];
+            if (res != -1) return res;
+            string temp;
+            res = 0;
+            for (int i = idx; i < s.size(); i++) {
+                temp += s[i];
+                if (mp.count(temp) > 0 && dfs(i + 1)) {
+                    res = 1;
+                    break;
+                }
             }
-            return ans;
+            return res;
         };
-        auto res = dfs(0);
-        return (bool)res;
+        return dfs(0);
     }
 };
