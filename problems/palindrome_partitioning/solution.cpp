c76ae8b1a776b3342a03bf8caf9de956f6293d34
Saurav Chandra <sauravchandra125@gmail.com>
2021-05-03 06:53:43 +0000

    Sync LeetCode submission - Palindrome Partitioning (cpp)

--- problems/palindrome_partitioning/solution.cpp
+++ problems/palindrome_partitioning/solution.cpp
@@ -2,29 +2,44 @@ class Solution {
 public:
     vector<vector<string>> partition(string s) {
         int len = s.size();
-        vector<vector<string>> ans;
-        vector<string> res;
-        auto chk = [&](string str) {
-            auto cpy = str;
-            reverse(str.begin(), str.end());
-            if (cpy == str) return true;
-            else return false;
-        };
-        function<void(string, int)> dfs = [&](string str, int idx) {
-            if (idx == len) {
-                ans.push_back(res);
-                return ;
+        vector<vector<bool>> 
+            palindrome(len + 1, vector<bool> (len + 1, false));
+        
+        function<bool(string)> isPalin = [&](string str) {
+            int l = 0, r = str.size() - 1;
+            while (l < r) {
+                if (str[l] != str[r]) return false;
+                l++, r--;
             }
+            return true;
+        };
+        
+        string temp;
+        for (int i = 0; i < len; i++) {
+            temp = "";
+            for (int j = i; j < len; j++) {
+                temp += s[j];
+                palindrome[i][j] = isPalin(temp);
+            }
+        }
+        vector<vector<string>> ans;
+        vector<string> vec;
+        function<void(int)> dfs = [&](int idx) {
+            if (idx == len) {
+                ans.push_back(vec);
+                return;
+            }
+            string temp;
             for (int i = idx; i < len; i++) {
-                str += s[i];
-                if (chk(str)) {
-                    res.push_back(str);
-                    dfs("", i + 1);
-                    res.pop_back();
+                temp += s[i];
+                if (palindrome[idx][i]) {
+                    vec.push_back(temp);
+                    dfs(i + 1);
+                    vec.pop_back();
                 }
             }
         };
-        dfs("", 0);
+        dfs(0);
         return ans;
     }
 };
