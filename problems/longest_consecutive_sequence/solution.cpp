8c183589634749c4a966c653d5f8d295c7cc6225
Saurav Chandra <sauravchandra125@gmail.com>
2021-05-03 12:27:16 +0000

    Sync LeetCode submission - Longest Consecutive Sequence (cpp)

--- problems/longest_consecutive_sequence/solution.cpp
+++ problems/longest_consecutive_sequence/solution.cpp
@@ -1,22 +1,20 @@
 class Solution {
-    public:
-        int longestConsecutive(vector < int > & nums) {
-            int n = nums.size();
-            if (n == 0)
-                return 0;
-            int maxx = 1, longest_maxx = 1;
-            sort(nums.begin(), nums.end());
-            for (int i = 1; i < n; i++) {
-                if (nums[i] != nums[i - 1]) {
-                    if (nums[i] == (nums[i - 1] + 1)) {
-                        maxx += 1;
-                    } else {
-                        longest_maxx = max(longest_maxx, maxx);
-                        maxx = 1;
-                    }
-                }
-
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int len = nums.size();
+        sort(nums.begin(), nums.end());
+        int ans = min(len, 1);
+        int curr = ans;
+        for (int i = 1; i < len; i++) {
+            if (nums[i] == nums[i - 1]) continue;
+            if (nums[i] == nums[i - 1] + 1) {
+                curr++;
+            } else {
+                ans = max(ans, curr);
+                curr = 1;
             }
-            return max(maxx, longest_maxx);
         }
+        ans = max(ans, curr);
+        return ans;
+    }
 };
