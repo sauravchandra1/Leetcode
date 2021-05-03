e3674c4cdd9088941082b1adfa72e2fe4f98b6e7
Saurav Chandra <sauravchandra125@gmail.com>
2021-05-03 12:16:54 +0000

    Sync LeetCode submission - 4Sum (cpp)

--- problems/4sum/solution.cpp
+++ problems/4sum/solution.cpp
@@ -1,37 +1,32 @@
 class Solution {
-    public:
-        vector < vector < int >> fourSum(vector < int > & nums, int target) {
-            vector < vector < int >> output;
-            sort(nums.begin(), nums.end());
-            int n = nums.size();
-            set < string > check;
-            for (int i = 0; i < n - 3; i++) {
-                for (int j = i + 1; j < n - 2; j++) {
-                    int low = j + 1;
-                    int high = n - 1;
-                    while (high > low) {
-                        int current = nums[i] + nums[j] + nums[low] + nums[high];
-                        if (current == target) {
-                            string s = to_string(nums[i]) + to_string(nums[j]) +
-                                to_string(nums[low]) + to_string(nums[high]);
-                            if (check.find(s) == check.end()) {
-                                check.insert(s);
-                                output.push_back({
-                                    nums[i],
-                                    nums[j],
-                                    nums[low],
-                                    nums[high]
-                                });
-                            }
-                            low++, high--;
-                        } else if (current > target) {
-                            high--;
-                        } else {
-                            low++;
-                        }
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int len = nums.size();
+        unordered_map<int, int> mp;
+        set<vector<int>> se;
+        vector<int> vec(4, 0);
+        vector<vector<int>> ans;
+        int a, b, c, d;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < len; i++) mp[nums[i]] = i;
+        for (int i = 0; i < len; i++) {
+            a = nums[i];
+            vec[0] = a;
+            for (int j = i + 1; j < len; j++) {
+                b = nums[j];
+                vec[1] = b;
+                for (int k = j + 1; k < len; k++) {
+                    c = nums[k];
+                    vec[2] = c;
+                    d = target - (a + b + c);
+                    if (mp[d] > k) {
+                        vec[3] = d;
+                        se.insert(vec);
                     }
                 }
             }
-            return output;
         }
+        for (auto v : se) ans.push_back(v);
+        return ans;
+    }
 };
