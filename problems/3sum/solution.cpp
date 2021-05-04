1cab0450998097d71dfb7b50c00e6e1f75872c63
Saurav Chandra <sauravchandra125@gmail.com>
2021-05-04 09:18:09 +0000

    Sync LeetCode submission - 3Sum (cpp)

--- problems/3sum/solution.cpp
+++ problems/3sum/solution.cpp
@@ -1,94 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<int> pos, neg, zero;
-        unordered_map<int, int> pp, nn;
-        for (auto v : nums) {
-            if (v == 0) zero.push_back(v);
-            else if (v > 0) {
-                pos.push_back(v);
-                pp[v]++;
-            }
-            else { 
-                neg.push_back(v);
-                nn[v]++;
-            }
-        }
-        bool ok = false;
-        if (zero.size() > 0) ok = true;
-        sort(neg.begin(), neg.end(), greater<int>());
-        sort(pos.begin(), pos.end());
+        sort(nums.begin(), nums.end());
+        unordered_map<int, int> mp;
+        int len = nums.size();
+        for (int i = 0; i < len; i++) mp[nums[i]] = i;
+        vector<int> vec(3);
         vector<vector<int>> ans;
-        set<vector<int>> res;
-        vector<int> tt;
-        if (zero.size() >= 3) ans.push_back({0, 0, 0});
-        for (auto p : pos) {
-            for (auto n : neg) {
-                if (p == -n && ok) {
-                    tt.clear();
-                    tt.push_back(n);
-                    tt.push_back(0);
-                    tt.push_back(p);
-                    if (res.find(tt) == res.end()) res.insert(tt);
-                } else if (p < -n) {
-                    break;
-                } else {
-                    int r = p + n;
-                    r = -r;
-                    if (r == n) {
-                        if (nn[r] >= 2) {
-                            tt.clear();
-                            tt.push_back(r);
-                            tt.push_back(r);
-                            tt.push_back(p);
-                            if (res.find(tt) == res.end()) res.insert(tt);
-                        }
-                    } else {
-                        if (nn[r] >= 1) {
-                            tt.clear();
-                            tt.push_back(min(r, n));
-                            tt.push_back(max(r, n));
-                            tt.push_back(p);
-                            if (res.find(tt) == res.end()) res.insert(tt);
-                        }
-                    }
+        for (int i = 0; i < len - 2; i++) {
+            vec[0] = nums[i];
+            for (int j = i + 1; j < len - 1; j++) {
+                vec[1] = nums[j];
+                int sum = vec[0] + vec[1];
+                if (mp.count(-sum) && mp[-sum] > j) {
+                    vec[2] = -sum;
+                    ans.push_back(vec);
                 }
+                while (j < len - 1 && nums[j] == nums[j + 1]) j++;
             }
+            while (i < len - 2 && nums[i] == nums[i + 1]) i++;
         }
-         for (auto n : neg) {
-            for (auto p : pos) {
-                if (p == -n && ok) {
-                    tt.clear();
-                    tt.push_back(n);
-                    tt.push_back(0);
-                    tt.push_back(p);
-                    if (res.find(tt) == res.end()) res.insert(tt);
-                } else if (p > -n) {
-                    break;
-                } else {
-                    int r = p + n;
-                    r = -r;
-                    if (r == p) {
-                        if (pp[r] >= 2) {
-                            tt.clear();
-                            tt.push_back(n);
-                            tt.push_back(r);
-                            tt.push_back(r);
-                            if (res.find(tt) == res.end()) res.insert(tt);
-                        }
-                    } else {
-                        if (pp[r] >= 1) {
-                            tt.clear();
-                            tt.push_back(n);
-                            tt.push_back(min(r, p));
-                            tt.push_back(max(r, p));
-                            if (res.find(tt) == res.end()) res.insert(tt);
-                        }
-                    }
-                }
-            }
-        }
-        for (auto v : res) ans.push_back(v);
         return ans;
     }
 };
