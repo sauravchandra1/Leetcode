0d8f8e989d86bdeb45227ef598668edd48ba5a61
Saurav Chandra <sauravchandra125@gmail.com>
2021-05-04 05:50:29 +0000

    Sync LeetCode submission - Add Two Numbers (cpp)

--- problems/add_two_numbers/solution.cpp
+++ problems/add_two_numbers/solution.cpp
@@ -11,59 +11,28 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        auto get = [&](int a, int b, int c) {
-            pair<int, int> pp;
-            pp.first = (a + b + c) % 10;
-            pp.second = (a + b + c) / 10;
-            return pp;
-        };
-        auto get_len = [&](ListNode* head) {
-            int len = 0;
-            ListNode* last = NULL;
-            while (head != NULL) {
-                last = head;
-                head = head->next;
-                len++;
+        ListNode* head = 0, *curr = 0;
+        head = new ListNode(0);
+        curr = head;
+        int sum = 0, carry = 0;
+        while (l1 || l2) {
+            sum = 0;
+            if (l1) sum += l1->val;
+            if (l2) sum += l2->val;
+            sum += carry;
+            carry = 0;
+            if (sum >= 10) {
+                sum -= 10;
+                carry++;
             }
-            last->next = new ListNode(0);
-            return len;
-        };
-        auto add = [&](ListNode* head, int cnt) {
-            while (head->next != NULL) head = head->next;
-            while (cnt--) {
-                head->next = new ListNode(0);
-                head = head->next;
-            }
-        };
-        ListNode* root = NULL;
-        auto remove_zero = [&](ListNode* head) {
-            ListNode* last = NULL;
-            while (head != NULL) {
-                if (head->val != 0) last = head;
-                head = head->next;
-            }
-            if (last != NULL) last->next = NULL;
-            else root->next = NULL;
-        };
-        int len1 = get_len(l1), len2 = get_len(l2);
-        if (len1 > len2) add(l2, len1 - len2);
-        else add(l1, len2 - len1);
-        ListNode* curr = l1;
-        pair<int, int> pp;
-        ListNode* tt = NULL;
-        int cc = 0;
-        while (l1 != NULL) {
-            pp = get(l1->val, l2->val, cc);
-            tt = new ListNode(0);
-            tt->val = pp.first;
-            cc = pp.second;
-            if (curr == l1) root = tt;
-            else curr->next = tt;
-            curr = tt;
-            l1 = l1->next;
-            l2 = l2->next;
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+            curr->next = new ListNode(sum);
+            curr = curr->next;
         }
-        remove_zero(root);
-        return root;
+        if (carry) {
+            curr->next = new ListNode(carry);
+        }
+        return head->next;
     }
 };
