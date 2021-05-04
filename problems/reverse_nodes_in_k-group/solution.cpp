a8542321cfc1be2a13fb45f6c7ac4989db66ce18
Saurav Chandra <sauravchandra125@gmail.com>
2021-05-04 08:56:20 +0000

    Sync LeetCode submission - Reverse Nodes in k-Group (cpp)

--- problems/reverse_nodes_in_k-group/solution.cpp
+++ problems/reverse_nodes_in_k-group/solution.cpp
@@ -1,23 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
-    public:
-        ListNode * reverseKGroup(ListNode * head, int k) {
-            ListNode * temp = head;
-            for (int i = 0; i < k; i++) {
-                if (!temp) return head;
-                temp = temp -> next;
-            }
-
-            ListNode * prev = NULL;
-            ListNode * nex1 = NULL;
-            temp = head;
-            for (int i = 0; i < k; i++) {
-                nex1 = temp -> next;
-                temp -> next = prev;
-                prev = temp;
-                temp = nex1;
-            }
-            if (nex1 != NULL)
-                head -> next = reverseKGroup(nex1, k);
-            return prev;
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        int len = 0;
+        ListNode* curr = head;
+        while (curr) {
+            curr = curr->next;
+            len++;
         }
+        len = len - len % k;
+        len /= k;
+        ListNode* next = 0, *temp = 0, *newHead = 0, *tail = 0;
+        newHead = head;
+        bool ok = false;
+        while (len--) { 
+            curr = newHead;
+            next = curr->next;
+            for (int i = 1; i < k; i++) {
+                temp = next->next;
+                next->next = curr;
+                curr = next;
+                next = temp;
+            }
+            if (tail) tail->next = curr;
+            tail = newHead;
+            newHead = next;
+            if (!ok) {
+                head = curr;
+                ok = true;
+            }
+        }
+        if (tail) tail->next = next;
+        return head;
+    }
 };
