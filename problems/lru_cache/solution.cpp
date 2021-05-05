b640d41d8ef4b1c2e5fc0131121f55838e65f683
Saurav Chandra <sauravchandra125@gmail.com>
2021-05-05 06:02:57 +0000

    Sync LeetCode submission - LRU Cache (cpp)

--- problems/lru_cache/solution.cpp
+++ problems/lru_cache/solution.cpp
@@ -1,71 +1,40 @@
-class Node {
-public:
-    int key;
-    Node* next;
-    Node* prev;
-    
-    Node(int _key) {
-        key = _key;
-        next = NULL;
-        prev = NULL;
-    }
-};
-
 class LRUCache {
 public:
-    Node* head = NULL;
-    Node* tail = NULL;
-    map<int, pair<Node*, int>> mp;
-    int cap;
-    int curr;
+    unordered_map<int, int> timer, p;
+    map<int, int> mp;
+    int currTimer, cap;
     LRUCache(int capacity) {
+        currTimer = 0;
         cap = capacity;
-        head = new Node(0);
-        tail = new Node(0);
-        head->next = tail;
-        tail->prev = head;
-        curr = 0;
-    }
-    
-    void remove(Node* node) {
-        // Remove Node from the list
-        Node* prev = node->prev;
-        Node* next = node->next;
-        prev->next = next;
-        next->prev = prev;
-        int key = node->key;
-        mp[key] = {NULL, -1};
-    }
-    
-    void insert(int key, int val) {
-        Node* temp = new Node(key);
-        temp->next = head->next;
-        (head->next)->prev = temp;
-        head->next = temp;
-        temp->prev = head;
-        mp[key] = make_pair(temp, val);
-    }
-    
-    void set(Node* node, int key, int val) {
-        // Remove Node from the list
-        remove(node);
-        // Create a new node in the beginning
-        insert(key, val);
     }
     
     int get(int key) {
-        if (mp.count(key) == 0 || mp[key].second == -1) return -1;
-        set(mp[key].first, key, mp[key].second);
-        return mp[key].second;
+        currTimer++;
+        if (p.count(key) > 0) {
+            int t = timer[key];
+            mp.erase(t);
+            mp[currTimer] = key;
+            timer[key] = currTimer;
+            return p[key];
+        } return -1;
     }
     
     void put(int key, int value) {
-        bool ok = false;
-        if (mp.count(key) == 0) ok = true;
-        if (mp.count(key) > 0 && mp[key].second != -1) set(mp[key].first, key, value);
-        else if (curr >= cap) set(tail->prev, key, value);
-        else insert(key, value);
-        curr += (ok ? 1 : 0);
+        currTimer++;
+        if (p.count(key) > 0) {
+            p[key] = value;
+            int t = timer[key];
+            mp.erase(t);
+        } else if (mp.size() == cap) {
+            auto it = mp.begin();
+            int t = it->first, k = it->second;
+            p.erase(k);
+            timer.erase(k);
+            mp.erase(t);
+        } 
+        timer[key] = currTimer;
+        p[key] = value;
+        mp[currTimer] = key;
     }
 };
 
