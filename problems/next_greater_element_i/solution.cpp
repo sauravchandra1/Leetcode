05732f7bf1e9c2486bd4cd1e041d8dcddb1b5c1b
Saurav Chandra <sauravchandra125@gmail.com>
2021-05-05 05:17:34 +0000

    Sync LeetCode submission - Next Greater Element I (cpp)

--- problems/next_greater_element_i/solution.cpp
+++ problems/next_greater_element_i/solution.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
-     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> m;
-        for (int i = 0; i < nums2.size(); ++i) {
-            m.insert(make_pair(nums2[i], i));
-        }
-        for (int i = 0; i < nums1.size(); ++i) {
-            const int iVal = nums1[i];
-            unordered_map<int, int>::const_iterator it = m.find(iVal);
-            if (it == m.cend()) {
-                nums1[i] = -1;
-            } else {
-                bool found = false;
-                const int nums2Idx = it->second;
-                for (int j = nums2Idx + 1; j < nums2.size(); ++j) {
-                    const int jVal = nums2[j];
-                    if (jVal > iVal) {
-                        nums1[i] = jVal;
-                        found = true;
-                        break;
-                    }
-                }
-                if (!found) {
-                    nums1[i] = -1;
-                }
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int len = nums2.size();
+        stack<int> stk;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < len; i++) {
+            while (!stk.empty() && stk.top() < nums2[i]) {
+                int val = stk.top();
+                stk.pop();
+                mp[val] = nums2[i];
             }
+            stk.push(nums2[i]);
+        }
+        for (int i = 0; i < nums1.size(); i++) {
+            if (mp.count(nums1[i]) > 0) nums1[i] = mp[nums1[i]];
+            else nums1[i] = -1;
         }
         return nums1;
     }
