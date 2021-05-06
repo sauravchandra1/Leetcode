49304b794ad5db0f3625ce62f00eb21850f6052c
Saurav Chandra <sauravchandra125@gmail.com>
2021-05-06 04:32:07 +0000

    Sync LeetCode submission - Sliding Window Maximum (cpp)

--- problems/sliding_window_maximum/solution.cpp
+++ problems/sliding_window_maximum/solution.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        multiset<int> ml;
+        deque<pair<int, int>> dq;
         int len = nums.size();
+        auto get = [&](int i) {
+            while (!dq.empty() && 
+                   dq.front().second < (i - k + 1)) dq.pop_front();
+            if (dq.empty()) dq.push_back({nums[i], i});
+            else {
+                while (!dq.empty() && 
+                       dq.back().first <= nums[i]) dq.pop_back();
+                dq.push_back({nums[i], i});
+            }
+        };
+        for (int i = 0; i < k - 1; i++) get(i);
         vector<int> ans;
-        for (int i = 0; i < k; i++) ml.insert(nums[i]);
-        ans.push_back(*ml.rbegin());
-        auto it = ml.find(nums[0]);
-        ml.erase(it);
-        for (int i = k; i < len; i++) {
-            ml.insert(nums[i]);
-            ans.push_back(*ml.rbegin());
-            auto it = ml.find(nums[i - k + 1]);
-            ml.erase(it);
+        for (int i = k - 1; i < len; i++) {
+            get(i);
+            ans.push_back(dq.front().first);
         }
         return ans;
     }
