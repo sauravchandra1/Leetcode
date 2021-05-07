ca335b5b78c4c98163d4c28b69376e64a5850d77
Saurav Chandra <sauravchandra125@gmail.com>
2021-05-07 05:00:40 +0000

    Sync LeetCode submission - Number of Islands (cpp)

--- problems/number_of_islands/solution.cpp
+++ problems/number_of_islands/solution.cpp
@@ -1,36 +1,47 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = 0;
-        if (n) m = grid[0].size();
-        int c = 0;
-        auto valid = [&](int x, int y) {
-            if (x < 0 || x >= n || y < 0 || y >= m) return false;
+        int r = grid.size();
+        int c = grid[0].size();
+        int ans = 0;
+        
+        const int N = 4;
+        vector<int> dx {0, 1, 0, -1};
+        vector<int> dy {1, 0, -1, 0};
+        
+        auto isFine = [&](int x, int y) {
+            if (x >= r || x < 0 || y >= c || y < 0) return false;
             else return true;
         };
-        vector<int> dx{0, 1, 0, -1};
-        vector<int> dy{1, 0, -1, 0};
-        const int N = 4;
+        
         function<void(int, int)> dfs = [&](int x, int y) {
-            grid[x][y] = '#';
-            int xx, yy;
-            for (int i = 0; i < N; i++) {
-                xx = dx[i] + x;
-                yy = dy[i] + y;
-                if (valid(xx, yy) && grid[xx][yy] == '1') {
-                    dfs(xx, yy);
+            queue<pair<int, int>> Q;
+            Q.push({x, y});
+            grid[x][y] = '0';
+            while (!Q.empty()) {
+                int x = Q.front().first, y = Q.front().second;
+                Q.pop();
+                for (int i = 0; i < N; i++) {
+                    int xx = x + dx[i];
+                    int yy = y + dy[i];
+                    if (isFine(xx, yy) && grid[xx][yy] == '1') {
+                        Q.push({xx, yy});
+                        grid[xx][yy] = '0';
+                    }
                 }
+
             }
         };
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        
+        
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
                 if (grid[i][j] == '1') {
-                    c++;
+                    ans++;
                     dfs(i, j);
                 }
             }
         }
-        return c;
+        return ans;
     }
 };
