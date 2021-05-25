e00479ad858755971c3a3c3f8b89d33468315680
Saurav Chandra <sauravchandra125@gmail.com>
2021-05-25 07:28:49 +0000

    Sync LeetCode submission - Evaluate Reverse Polish Notation (cpp)

--- problems/evaluate_reverse_polish_notation/solution.cpp
+++ problems/evaluate_reverse_polish_notation/solution.cpp
@@ -1,31 +1,45 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        int ans = 0;
         stack<int> stk;
-        int len = tokens.size();
-        int a, b;
-        if (len == 0) return 0;
-        for (auto v : tokens) {
-            if (v.size() == 1 && v[0] < '0') {
-                b = stk.top();
-                stk.pop();
-                a = stk.top();
-                stk.pop();
-                if (v[0] == '+') {
-                    stk.push(a + b);
-                } else if (v[0] == '-') {
-                    stk.push(a - b);
-                } else if (v[0] == '*') {
-                    stk.push(a * b);
-                } else if (v[0] == '/') {
-                    stk.push(a / b);
-                } 
+        auto isOperation = [&](string str) {
+            if (str[0] >= '0' && str[0] <= '9' || str.size() > 1) return false;
+            else return true;
+        };
+        auto getInt = [&](string str) {
+            int num = 0;
+            int neg = 1;
+            for (auto& c : str) {
+                if (c == '-') neg = -1;
+                else num = num * 10 + (c - '0');
             }
-            else {
-                if (v[0] == '-') stk.push(stoi(v.substr(1)) * -1);
-                else stk.push(stoi(v));
-            }
+            num *= neg;
+            return num;
+        };
+        for (auto& c : tokens) {
+            if (isOperation(c)) {
+                int b = stk.top();
+                stk.pop();
+                int a = stk.top();
+                stk.pop();
+                int res = 0;
+                char op = c[0];
+                switch(op) {
+                    case '+':
+                        res = a + b;
+                        break;
+                    case '-':
+                        res = a - b;
+                        break;
+                    case '/':
+                        res = a / b;
+                        break;
+                    default:
+                        res = a * b;
+                        break;
+                }
+                stk.push(res);
+            } else stk.push(getInt(c));
         }
         return stk.top();
     }
