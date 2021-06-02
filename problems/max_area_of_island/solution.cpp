47cf2fe942cac6de261ff5bf80042c64ac111fb1
Saurav Chandra <sauravchandra125@gmail.com>
2021-06-02 05:00:20 +0000

    Sync LeetCode submission - Max Area of Island (cpp)

--- problems/max_area_of_island/solution.cpp
+++ problems/max_area_of_island/solution.cpp
@@ -2,44 +2,36 @@ class Solution {
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int n = grid.size();
-        if (n == 0) return 0;
         int m = grid[0].size();
-        vector<int> dx {0, 1, 0, -1};
-        vector<int> dy {1, 0, -1, 0};
-        auto go = [&](int x, int y) {
-            if (x < 0 || x >= n || y < 0 || y >= m) return false;
-            if (grid[x][y] != 1) return false;
+        queue<pair<int, int>> Q;
+        int ans = 0;
+        auto isFine = [&](int x, int y) {
+            if (x < 0 || y < 0 || x >= n || y >= m) return false;
             return true;
         };
-        function<int(int, int)> dfs = [&](int x, int y) {
-            queue<pair<int, int>> Q;
-            Q.push({x, y});
-            grid[x][y] = 0;
-            int d = 0;
-            while (!Q.empty()) {
-                int len = Q.size();
-                for (int i = 0; i < len; i++) {
-                    int x = Q.front().first;
-                    int y = Q.front().second;
-                    Q.pop();
-                    d++;
-                    for (int i = 0; i < 4; i++) {
-                        int _x = x + dx[i];
-                        int _y = y + dy[i];
-                        if (go(_x, _y)) {
-                            Q.push({_x, _y});
-                            grid[_x][_y] = 0;
-                        }
-                    }
-                }
-            }    
-            return d;
-        };
-        int ans = 0;
+        vector<int> dx {1, 0, -1, 0};
+        vector<int> dy {0, 1, 0, -1};
+        const int N = 4;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[i][j]) {
-                    ans = max(ans, dfs(i, j));
+                if (grid[i][j] == 1) {
+                    Q.push({i, j});
+                    grid[i][j] = 0;
+                    int res = 0;
+                    while (!Q.empty()) {
+                        int x = Q.front().first, y = Q.front().second;
+                        res++;
+                        Q.pop();
+                        for (int k = 0; k < N; k++) {
+                            int _x = x + dx[k];
+                            int _y = y + dy[k];
+                            if (isFine(_x, _y) && grid[_x][_y] == 1) {
+                                Q.push({_x, _y});
+                                grid[_x][_y] = 0;
+                            }
+                        }
+                    }
+                    ans = max(ans, res);
                 }
             }
         }
