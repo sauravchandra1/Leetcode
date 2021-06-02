95314d9e35be3c8886f77f03a1d1bef5839867fe
Saurav Chandra <sauravchandra125@gmail.com>
2021-06-02 08:32:28 +0000

    Sync LeetCode submission - Interleaving String (cpp)

--- problems/interleaving_string/solution.cpp
+++ problems/interleaving_string/solution.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        int len = s3.size();
-        int n = s1.size();
-        int m = s2.size();
-        if (n + m != len) return false;
-        vector<vector<int>> dp(n + 1, vector<int> (m + 1, -1));
-        function<int(int, int)> dfs = [&](int l1, int l2) {
-            if (l1 == n && l2 == m) return 1;
-            auto &res = dp[l1][l2];
-            if (res != -1) return res;
+        int len1 = s1.size();
+        int len2 = s2.size();
+        int len3 = s3.size();
+        if (len1 + len2 != len3) return false;
+        if (len1 == 0) return (s2 == s3);
+        if (len2 == 0) return (s1 == s3);
+        int dp[len1][len3][len2 + 1];
+        for (int i = 0; i < len1; i++)
+            for (int j = 0; j < len3; j++) 
+                for (int k = 0; k < len2 + 1; k++) 
+                    dp[i][j][k] = -1;
+        function<bool(int, int, int)> dfs = [&](int l1, int l3, int l2) {
+            if (l1 == len1) {
+                while (l3 < len3 && l2 < len2) {
+                    if (s3[l3] != s2[l2]) return false;
+                    l2++, l3++;
+                }
+                return (l2 == len2);
+            };
+            if (l3 == len3) return false;
+            auto& res = dp[l1][l3][l2];
+            if (res != -1) return (bool)res;
             res = 0;
-            int l3 = l1 + l2;
-            int a = 0, b = 0;
-            if (l1 < n && l3 < len && s3[l3] == s1[l1]) {
-                a = dfs(l1 + 1, l2);
-            }
-            if (l2 < m && l3 < len && s3[l3] == s2[l2]) {
-                b = dfs(l1, l2 + 1);
-            }
-            res = max(a, b);
-            return res;
+            if (s1[l1] == s3[l3]) res = dfs(l1 + 1, l3 + 1, l2);
+            res = res || dfs(l1, l3 + 1, l2 + (s3[l3] == s2[l2]));
+            return (bool)res;
         };
-        int ans = dfs(0, 0);
-        if (ans == 1) return true;
-        else return false;
+        return dfs(0, 0, 0);
     }
 };
