21d9a084d2efc923658c72ffd2dc5b977da34715
Saurav Chandra <sauravchandra125@gmail.com>
2021-06-08 12:08:43 +0000

    Sync LeetCode submission - Construct Binary Tree from Preorder and Inorder Traversal (cpp)

--- problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.cpp
+++ problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.cpp
@@ -11,28 +11,21 @@
  */
 class Solution {
 public:
-    int idx = 0;
-    map<int ,int> mp;
-    TreeNode* trav(vector<int>& preorder, vector<int>& inorder, int l, int r) {
-        TreeNode* root = NULL;
-        if (l > r || idx == inorder.size()) return NULL;
-        if (l == r) {
-            root = new TreeNode(preorder[idx]);
-            idx++;
-        }
-        else {
-            int i = mp[preorder[idx]];
-            root = new TreeNode(preorder[idx]);
-            idx++;
-            root->left = trav(preorder, inorder, l, i - 1);
-            root->right = trav(preorder, inorder, i + 1, r);
-        }
-        return root;
-    }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        for (int i = 0; i < inorder.size(); i++) {
-            mp[inorder[i]] = i;
-        }
-        return trav(preorder, inorder, 0, inorder.size() - 1);
+        int len = inorder.size();
+        unordered_map<int, int> mp;
+        for (int i = 0; i < len; i++) mp[inorder[i]] = i;
+        int idx = 0;
+        function<TreeNode*(int, int)> dfs = [&](int l, int r) {
+            TreeNode* root = 0;
+            if (l > r) return root;
+            root = new TreeNode(preorder[idx++]);
+            if (l == r) return root;
+            int id = mp[preorder[idx - 1]];
+            root->left = dfs(l, id - 1);
+            root->right = dfs(id + 1, r);
+            return root;
+        };
+        return dfs(0, len - 1);
     }
 };
