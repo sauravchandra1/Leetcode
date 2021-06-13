14ab70141aab892aa290789603dc387ebfdcf292
Saurav Chandra <sauravchandra125@gmail.com>
2021-06-13 16:56:50 +0000

    Sync LeetCode submission - Palindrome Pairs (cpp)

--- problems/palindrome_pairs/solution.cpp
+++ problems/palindrome_pairs/solution.cpp
@@ -1,108 +1,67 @@
+const int N = 26;
+
+class TrieNode {
+public:    
+    vector<int> ind;
+    TrieNode* next[N];
+    TrieNode() { for (int i = 0; i < N; i++) next[i] = 0; }
+};
+
+class Trie {
+public:
+    TrieNode* root;
+    Trie() { root = new TrieNode(); }
+    void insert(string str, int i) {
+        TrieNode* curr = root;
+        for (auto c : str) {
+            c = c - 'a';
+            if (!curr->next[c])
+                curr->next[c] = new TrieNode();
+            curr = curr->next[c];
+        }
+        curr->ind.push_back(i);
+    }
+    vector<int> search(string str, int l, int r) {
+        TrieNode* curr = root;
+        char c;
+        for (int i = r; i >= l; i--) {
+            c = str[i] - 'a';
+            if (!curr->next[c]) return {};
+            curr = curr->next[c];
+        }
+        return curr->ind;
+    }
+};
+
 class Solution {
 public:
-    vector < vector < int >> res;
-
-    struct TrieNode {
-        int indexWord;
-        bool isWord;
-        TrieNode * children[26];
-    };
-
-    TrieNode * getNewNode() {
-        TrieNode * node = new TrieNode();
-        node -> isWord = false;
-        node -> indexWord = -1;
-        for (int i = 0; i < 26; i++)
-            node -> children[i] = NULL;
-        return node;
-    }
-
-    void insert(TrieNode * root, string & word, int indexWord) {
-        TrieNode * temp = root;
-        for (int i = 0; i < word.length(); i++) {
-            int index = word[i] - 'a';
-            if (temp -> children[index] == NULL)
-                temp -> children[index] = getNewNode();
-            temp = temp -> children[index];
+    bool isPalin(string str, int l, int r) {
+        while (l < r) {
+            if (str[l] != str[r]) return false;
+            l++, r--;
         }
-        temp -> isWord = true;
-        temp -> indexWord = indexWord;
+        return true;
     }
-
-    bool isSubstringPalindrone(string str) {
-        string s1 = str;
-        reverse(str.begin(), str.end());
-        return s1 == str;
-    }
-
-    void dfs(TrieNode * root, vector < int > & v1, string & str) {
-        if (root == NULL)
-            return;
-        if (root -> isWord == true && str != "" && isSubstringPalindrone(str))
-            v1.push_back(root -> indexWord);
-        for (int i = 0; i < 26; i++) {
-            if (root -> children[i] != NULL) {
-                str += 'a' + i;
-                dfs(root -> children[i], v1, str);
-                str.pop_back();
-            }
-        }
-    }
-
-    void helper(TrieNode * root, string word, int wordIndex) {
-        int index = 0;
-        while (index < word.length()) {
-            root = root -> children[word[index] - 'a'];
-            index++;
-            if (root == NULL)
-                break;
-            if (root -> isWord == true) {
-                if (root -> indexWord != wordIndex && isSubstringPalindrone(word.substr(index))) {
-                    vector < int > v1;
-                    v1.push_back(wordIndex);
-                    v1.push_back(root -> indexWord);
-                    res.push_back(v1);
+    vector<vector<int>> palindromePairs(vector<string>& words) {
+        int len = words.size();
+        Trie T;
+        for (int i = 0; i < len; i++) T.insert(words[i], i);
+        int idx = 0;
+        set<vector<int>> ans;
+        for (auto& w : words) {
+            if (!w.size()) { idx++; continue; };
+            for (int i = 0; i <= w.size(); i++) {
+                if (isPalin(w, i, w.size() - 1)) {
+                    auto vec = T.search(w, 0, i - 1);
+                    for (auto& v : vec) if (v != idx) ans.insert({idx, v});
+                }
+                if (isPalin(w, 0, i - 1)) {
+                    auto vec = T.search(w, i, w.size() - 1);
+                    for (auto& v : vec) if (v != idx) ans.insert({v, idx});
                 }
             }
+            idx++;
         }
-        if (index == word.length() && root != NULL) {
-            string str = "";
-            vector < int > v1;
-            dfs(root, v1, str);
-            for (int i: v1) {
-                if (i != wordIndex) {
-                    vector < int > v2;
-                    v2.push_back(wordIndex);
-                    v2.push_back(i);
-                    res.push_back(v2);
-                }
-            }
-        }
-
-    }
-
-    vector < vector < int >> palindromePairs(vector < string > & words) {
-        TrieNode * root = getNewNode();
-        int emptyIndex = -1;
-        for (int i = 0; i < words.size(); i++) {
-            if (words[i] == "")
-                emptyIndex = i;
-            string word = words[i];
-            reverse(word.begin(), word.end());
-            insert(root, word, i);
-        }
-
-        for (int i = 0; i < words.size(); i++) {
-            string word = words[i];
-            if (emptyIndex != -1 && i != emptyIndex && isSubstringPalindrone(word)) {
-                vector < int > v1;
-                v1.push_back(i);
-                v1.push_back(emptyIndex);
-                res.push_back(v1);
-            }
-            helper(root, word, i);
-        }
-
-        return res;
+        return vector<vector<int>>(ans.begin(), ans.end());
     }
 };
