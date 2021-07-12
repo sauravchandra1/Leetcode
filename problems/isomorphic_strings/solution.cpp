49e60940109628f5304ff5aeffb2bf17e28f880f
Saurav Chandra <sauravchandra125@gmail.com>
2021-07-12 07:40:26 +0000

    Sync LeetCode submission - Isomorphic Strings (cpp)

--- problems/isomorphic_strings/solution.cpp
+++ problems/isomorphic_strings/solution.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char, char> mp, seen;
-        int i = 0;
-        for (auto v : s) {
-            if (mp.count(v) == 0) {
-                if (seen.count(t[i]) > 0) return false;
-                mp[v] = t[i];
-                seen[t[i]] = '*';
-            } else if (mp[v] != t[i]) return false;
-            i++;
+        unordered_map<char, char> mp;
+        unordered_map<char, bool> ump;
+        for (int i = 0; i < s.size(); i++) {
+            if (mp.count(s[i]) && mp[s[i]] == t[i]) continue;
+            if (!mp.count(s[i]) && !ump.count(t[i])) {
+                mp[s[i]] = t[i];
+                ump[t[i]] = true;
+            } else return false;
         }
         return true;
     }
