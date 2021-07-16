89de9bb7582ccf0916eb0b11d3dc546772127d4a
Saurav Chandra <sauravchandra125@gmail.com>
2021-07-16 07:23:54 +0000

    Sync LeetCode submission - 4Sum (cpp)

--- problems/4sum/solution.cpp
+++ problems/4sum/solution.cpp
@@ -2,31 +2,22 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int len = nums.size();
-        unordered_map<int, int> mp;
-        set<vector<int>> se;
-        vector<int> vec(4, 0);
-        vector<vector<int>> ans;
-        int a, b, c, d;
         sort(nums.begin(), nums.end());
+        unordered_map<int, int> mp;
         for (int i = 0; i < len; i++) mp[nums[i]] = i;
+        int val;
+        vector<vector<int>> ans;
         for (int i = 0; i < len; i++) {
-            a = nums[i];
-            vec[0] = a;
             for (int j = i + 1; j < len; j++) {
-                b = nums[j];
-                vec[1] = b;
                 for (int k = j + 1; k < len; k++) {
-                    c = nums[k];
-                    vec[2] = c;
-                    d = target - (a + b + c);
-                    if (mp[d] > k) {
-                        vec[3] = d;
-                        se.insert(vec);
-                    }
+                    val = target - (nums[i] + nums[j] + nums[k]);
+                    if (mp[val] > k) ans.push_back({nums[i], nums[j], nums[k], val});
+                    while (k + 1 < len && nums[k + 1] == nums[k]) k++;
                 }
+                while (j + 1 < len && nums[j + 1] == nums[j]) j++;
             }
+            while (i + 1 < len && nums[i + 1] == nums[i]) i++;
         }
-        for (auto v : se) ans.push_back(v);
         return ans;
     }
 };
