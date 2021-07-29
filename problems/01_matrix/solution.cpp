2d45e9de5d8835c42c5af5d86eee668d6da86f38
Saurav Chandra <sauravchandra125@gmail.com>
2021-07-29 07:09:54 +0000

    Sync LeetCode submission - 01 Matrix (cpp)

--- problems/01_matrix/solution.cpp
+++ problems/01_matrix/solution.cpp
@@ -1,55 +1,40 @@
 class Solution {
-    public:
-            vector < vector < int >> updateMatrix(vector < vector < int >> & matrix) {
-            int n, m;
-            n = matrix.size(), m = matrix[0].size();
-            vector < vector < int >> dp = vector < vector < int >> (n, vector < int > (m, n * m + 1));
-            int a[4] = {
-                -1,
-                0,
-                0,
-                1
-            }, b[4] = {
-                0,
-                1,
-                -1,
-                0
-            };
-            list < pair < int, int >> q;
-            for (int i = 0; i < n; ++i) {
-                for (int j = 0; j < m; ++j) {
-                    if (matrix[i][j] == 0) {
-                        q.push_back({
-                            i,
-                            j
-                        });
-                        dp[i][j] = 0;
-                    }
-                }
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        queue<pair<int, int>> Q;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0) Q.push({i, j});
+                else mat[i][j] = -1;
             }
-            for (int lev = 1; true; lev++) {
-                if (q.empty()) {
-                    return dp;
-                }
-                int qn = q.size(), curi, curj;
-                pair < int, int > cur;
-                while (qn--) {
-                    cur = q.front();
-                    q.pop_front();
-                    curi = cur.first, curj = cur.second;
-                    for (int i = 0; i < 4; i++) {
-                        int ii = curi + a[i], jj = curj + b[i];
-                        if (ii < 0 || ii >= n || jj < 0 || jj >= m) continue;
-                        if (dp[ii][jj] > lev) {
-                            dp[ii][jj] = lev;
-                            q.push_back({
-                                ii,
-                                jj
-                            });
-                        }
-                    }
-                }
-            }
-            return dp;
         }
+        const int N = 4;
+        vector<int> dx {0, 1, 0 ,-1};
+        vector<int> dy {1, 0, -1, 0};
+        auto isFine = [&](int x, int y) {
+            if (x < 0 || x >= n || y < 0 || y >= m) return false;
+            return true;
+        };
+        int d = 0;
+        while (!Q.empty()) {
+            int len = Q.size();
+            d++;
+            for (int l = 0; l < len; l++) {
+                auto p = Q.front();
+                Q.pop();
+                int x = p.first, y = p.second;
+                for (int i = 0; i < N; i++) {
+                    int xx = x + dx[i];
+                    int yy = y + dy[i];
+                    if (isFine(xx, yy) && mat[xx][yy] == -1) {
+                        mat[xx][yy] = d;
+                        Q.push({xx, yy});
+                    }
+                }
+            }
+        }
+        return mat;
+    }
 };
