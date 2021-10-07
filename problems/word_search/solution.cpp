99d8e72d43e7ef05ae93383f8954e603392309d5
Saurav Chandra <sauravchandra125@gmail.com>
2021-10-07 02:55:11 +0000

    Sync LeetCode submission - Word Search (cpp)

--- problems/word_search/solution.cpp
+++ problems/word_search/solution.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
-    Solution() {ios_base::sync_with_stdio(false); cin.tie(NULL);}
     bool exist(vector<vector<char>>& board, string word) {
-        vector<int> dx{0, 1, 0, -1};
-        vector<int> dy{1, 0, -1, 0};
-        int n = board.size(), m = board[0].size();
-        auto valid = [&](int x, int y) {
-            if (x < 0 || x >= n || y < 0 || y >= m) return false;
-            else return true;
-        };
+        int n = board.size();
+        int m = board[0].size();
+        vector<int> dx {0, 1, 0, -1};
+        vector<int> dy {1, 0, -1, 0};
         const int N = 4;
-        vector<vector<bool>> seen(n, vector<bool> (m, false));
-        bool res = false;
-        function<bool(int, int, int)> get = [&](int x, int y, int idx) {
-            if (res) return res;
-            seen[x][y] = true;
-            if (idx == word.size()) return (res = true);
-            int xx, yy;
+        int ll;
+        auto get = [&](int x, int y) {
+            if (x >= n || x < 0 || y >= m || y < 0 || board[x][y] != word[ll]) 
+                return false;
+            return true;
+        };
+        function<bool(int, int, int)> dfs = [&](int r, int c, int l) {
+            if (l == word.size()) return true;
             for (int i = 0; i < N; i++) {
-                xx = dx[i] + x;
-                yy = dy[i] + y;
-                if (valid(xx, yy) && (board[xx][yy] == word[idx]) && !seen[xx][yy]) {
-                    res = (res || get(xx, yy, idx + 1));
-                    seen[xx][yy] = false;
+                int x = dx[i] + r;
+                int y = dy[i] + c;
+                ll = l;
+                if (get(x, y)) {
+                    board[x][y] = '.';
+                    if (dfs(x, y, l + 1)) return true;
+                    board[x][y] = word[l];
                 }
             }
-            return res;
+            return false;
         };
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
             for (int j = 0; j < m; j++) {
-                if (board[i][j] == word[0]) get(i, j, 1);
-                if (res) return res;
-                seen[i][j] = false;
+                if (board[i][j] == word[0]) {
+                    board[i][j] = '.';
+                    if (dfs(i, j, 1)) return true;
+                    board[i][j] = word[0];
+                }
             }
-        }
-        return res;
+        return false;   
     }
 };
