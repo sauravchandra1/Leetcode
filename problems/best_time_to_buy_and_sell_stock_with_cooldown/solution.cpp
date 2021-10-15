c387828febfef2d5fabb9663335395acf49e7fb5
Saurav Chandra <sauravchandra125@gmail.com>
2021-10-15 12:50:45 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock with Cooldown (cpp)

--- problems/best_time_to_buy_and_sell_stock_with_cooldown/solution.cpp
+++ problems/best_time_to_buy_and_sell_stock_with_cooldown/solution.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        const int N = 3;
-        int n = prices.size();
-        vector<vector<int>> dp(n + 1, vector<int> (N, -1));
-        /* 
-        State 0 -> Cooldown
-        State 1 -> Buy
-        State 2 -> Sell
-        */
-        function<int(int, int)> dfs = [&](int idx, int state) {
-            if (idx == n) return 0;
-            auto &res = dp[idx][state];
+        int len = prices.size();
+        vector<int> dp(len, -1);
+        function<int(int)> dfs = [&](int idx) {
+            if (idx >= len - 1) return 0;
+            auto& res = dp[idx];
             if (res != -1) return res;
-            if (state == 1) {
-                int a = dfs(idx + 1, 1);
-                int b = dfs(idx + 1, 2) + prices[idx];
-                res = max(a, b);
-            } else if (state == 2) {
-                int a = dfs(idx + 1, 0);
-                res = max(0, a);
-            } else {
-                int a = dfs(idx + 1, 1) - prices[idx];
-                int b = dfs(idx + 1, 0);
-                res = max(a, b);
+            res = 0;
+            for (int i = idx + 1; i < len; i++) {
+                if (prices[i] > prices[idx]) 
+                    res = max(res, prices[i] - prices[idx] + dfs(i + 2));
+                res = max(res, dfs(i));
             }
             return res;
         };
-        return max(0, dfs(0, 0));
+        return dfs(0);
     }
 };
