4cbe11e9bfbed585b5cd369cfa298ba3627705df
Saurav Chandra <sauravchandra125@gmail.com>
2021-10-16 07:15:04 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock III (cpp)

--- problems/best_time_to_buy_and_sell_stock_iii/solution.cpp
+++ problems/best_time_to_buy_and_sell_stock_iii/solution.cpp
@@ -2,41 +2,22 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int len = prices.size();
-        int k = 2;
-        if (len == 0) return 0;
-        const int N = 2;
-        if (k >= len * 2) {
-            int ans = 0;
-              for(int i = 1 ; i < len; i++){
-                  if(prices[i] > prices[i - 1]){
-                      ans += prices[i] - prices[i - 1];
-                  }
-              }
-              return ans;
+        vector<int> ans(len + 1);
+        int _max = 0;
+        for (int i = len - 1; i >= 0; i--) {
+            if (_max > prices[i]) 
+                ans[i] = _max - prices[i];
+            _max = max(_max, prices[i]);
         }
-        int dp[len + 1][k + 1][N];
-        for (int i = 0; i <= len; i++) {
-            for (int j = 0; j <= k; j++) {
-                dp[i][j][0] = dp[i][j][1] = -1;
-            }
+        for (int i = len - 2; i >= 0; i--) 
+            ans[i] = max(ans[i], ans[i + 1]);
+        int _min = prices[0];
+        int res = 0;
+        for (int i = 1; i < len; i++) {
+            if (prices[i] > _min)
+                res = max(res, prices[i] - _min + ans[i + 1]);
+            _min = min(_min, prices[i]);
         }
-        function<int(int, int, int)> dfs = [&](int idx, int k, int state) {
-            if (k < 0) return 0;
-            if (idx == len) return 0;
-            auto &res = dp[idx][k][state];
-            if (res != -1) return res;
-            res = 0;
-            int a = 0, b = 0;
-            if (state == 0) {
-                a = dfs(idx + 1, k - 1, 1) - prices[idx];
-                b = dfs(idx + 1, k, 0);
-            } else {
-                a = dfs(idx + 1, k, 0) + prices[idx];
-                b = dfs(idx + 1, k, 1);
-            }
-            res = max(a, b);
-            return res;
-        };
-        return dfs(0, k, 0);
+        return max(res, ans[0]);
     }
 };
