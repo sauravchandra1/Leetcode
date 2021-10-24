b3d2895114df8258260946a5a81e468d104c1e07
Saurav Chandra <sauravchandra125@gmail.com>
2021-10-24 05:55:34 +0000

    Sync LeetCode submission - Count Complete Tree Nodes (cpp)

--- problems/count_complete_tree_nodes/solution.cpp
+++ problems/count_complete_tree_nodes/solution.cpp
@@ -12,15 +12,27 @@
 class Solution {
 public:
     int countNodes(TreeNode* root) {
-        int ans = 0;
-        if (root == NULL) return 0;
-        function<void(TreeNode*)> dfs = [&](TreeNode* root) {
-            if (root == NULL) return;
-            ans++;
-            dfs(root->left);
-            dfs(root->right);
+        if (!root) return 0;
+        int h = 1;
+        TreeNode* curr = root;
+        while (curr->left) {
+            curr = curr->left;
+            h++;
+        }
+        int ans = (1 << h) - 1;
+        bool ok = false;
+        function<void(TreeNode*, int)> dfs = [&](TreeNode* root, int height) {
+            if (ok) return;
+            if (height == h) {
+                if (!root) ans--;
+                else ok = true;
+                return;
+            };
+            dfs(root->right, height + 1);
+            if (ok) return;
+            dfs(root->left, height + 1);
         };
-        dfs(root);
+        dfs(root, 1);
         return ans;
     }
 };
