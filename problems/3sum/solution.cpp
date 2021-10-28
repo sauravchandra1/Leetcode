4b3fd25978d2057d151aabb349c59e4dc3ad3fa2
Saurav Chandra <sauravchandra125@gmail.com>
2021-10-28 08:12:51 +0000

    Sync LeetCode submission - 3Sum (cpp)

--- problems/3sum/solution.cpp
+++ problems/3sum/solution.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        unordered_map<int, int> mp;
         int len = nums.size();
-        for (int i = 0; i < len; i++) mp[nums[i]] = i;
-        vector<int> vec(3);
+        if (len < 3) return {};
+        sort(nums.begin(), nums.end());
+        unordered_map<int, bool> mp;
+        for (auto& v : nums) mp[v] = true;
+        int count_zero = upper_bound(nums.begin(), nums.end(), 0) -                                          lower_bound(nums.begin(), nums.end(), 0);
         vector<vector<int>> ans;
-        for (int i = 0; i < len - 2; i++) {
-            vec[0] = nums[i];
-            for (int j = i + 1; j < len - 1; j++) {
-                vec[1] = nums[j];
-                int sum = vec[0] + vec[1];
-                if (mp.count(-sum) && mp[-sum] > j) {
-                    vec[2] = -sum;
-                    ans.push_back(vec);
-                }
-                while (j < len - 1 && nums[j] == nums[j + 1]) j++;
+        if (count_zero >= 3) ans.push_back({0, 0, 0});
+        for (int i = 0; i < len; i++) {
+            if (nums[i] >= 0) break;
+            for (int j = i + 1; j < len; j++) {
+                if (nums[j] > 0) break;
+                int v = nums[i] + nums[j];
+                if (mp.count(-v)) 
+                    ans.push_back({nums[i], nums[j], -v});
+                while (j + 1 < len && nums[j] == nums[j + 1]) j++;
             }
-            while (i < len - 2 && nums[i] == nums[i + 1]) i++;
+            while (i + 1 < len && nums[i] == nums[i + 1]) i++;
+        }
+        for (int i = len - 1; i >= 0; i--) {
+            if (nums[i] <= 0) break;
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[j] <= 0) break;
+                int v = nums[i] + nums[j];
+                if (mp.count(-v)) 
+                    ans.push_back({nums[i], nums[j], -v});
+                while (j - 1 >= 0 && nums[j] == nums[j - 1]) j--;
+            }
+            while (i - 1 >= 0 && nums[i] == nums[i - 1]) i--;
         }
         return ans;
     }
