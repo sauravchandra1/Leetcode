da74c6858b648b62b3bc1ee3693b68d3928a6a4e
Saurav Chandra <sauravchandra125@gmail.com>
2021-10-29 05:32:50 +0000

    Sync LeetCode submission - Rotting Oranges (cpp)

--- problems/rotting_oranges/solution.cpp
+++ problems/rotting_oranges/solution.cpp
@@ -1,57 +1,42 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int ans = 0;
-        queue<pair<int, int>> Q;
-        int r = grid.size();
-        int c = grid[0].size();
-        
-        auto isFine = [&](int x, int y) {
-            if (x >= r || x < 0 || y >= c || y < 0) return false;
-            return true;
-        };
-        
+        int n = grid.size();
+        int m = grid[0].size();
         vector<int> dx {0, 1, 0, -1};
         vector<int> dy {1, 0, -1, 0};
-        
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (grid[i][j] == 2) Q.push({i, j});
-            }
-        }
-        
-        if (Q.empty()) {
-            for (int i = 0; i < r; i++) {
-                for (int j = 0; j < c; j++) {
-                    if (grid[i][j] == 1) return -1;
-                }
-            }
-            return 0;
-        }
-        
-        while (!Q.empty()) {
-            int len = Q.size();
-            ans++;
-            for (int p = 0; p < len; p++) {
-                int x = Q.front().first, y = Q.front().second;
-                Q.pop();
-                for (int i = 0; i < 4; i++) {
-                    int xx = x + dx[i];
-                    int yy = y + dy[i];
-                    if (isFine(xx, yy) && grid[xx][yy] == 1) {
-                        Q.push({xx, yy});
-                        grid[xx][yy] = 2;
+        const int N = 4;
+        auto get = [&](int x, int y) {
+            if (x >= n || x < 0 || y >= m || y < 0) return false;
+            return true;
+        };
+        queue<pair<int, int>> Q;
+        int d = 0;
+        auto dfs = [&]() {
+            while (!Q.empty()) {
+                int len = Q.size();
+                d++;
+                for (int i = 0; i < len; i++) {
+                    int x = Q.front().first, y = Q.front().second;
+                    Q.pop();
+                    for (int l = 0; l < N; l++) {
+                        int xx = x + dx[l];
+                        int yy = y + dy[l];
+                        if (get(xx, yy) && grid[xx][yy] == 1) {
+                            grid[xx][yy] = 2;
+                            Q.push({xx, yy});
+                        }
                     }
                 }
             }
-        }
-        
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
+        };
+        for (int i = 0; i < n; i++) 
+            for (int j = 0; j < m; j++) 
+                if (grid[i][j] == 2) Q.push({i, j});
+        dfs();
+        for (int i = 0; i < n; i++) 
+            for (int j = 0; j < m; j++)
                 if (grid[i][j] == 1) return -1;
-            }
-        }
-        
-        return ans - 1;
+        return max(0, d - 1);
     }
 };
