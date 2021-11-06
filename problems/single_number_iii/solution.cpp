14a284e5244306aa036bb918c459e2ac280f8091
Saurav Chandra <sauravchandra125@gmail.com>
2021-11-06 04:15:19 +0000

    Sync LeetCode submission - Single Number III (cpp)

--- problems/single_number_iii/solution.cpp
+++ problems/single_number_iii/solution.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int bit = 0;
-        for (auto v : nums) bit ^= v;
+        long bit = 0;
+        for (auto& v : nums) bit ^= v;
         bit = bit & (-bit);
         int a = 0, b = 0;
-        for (auto v : nums) {
+        for (auto& v : nums) {
             if (v & bit) a = a ^ v;
             else b = b ^ v;
         }
-        vector<int> ans{a, b};
-        return ans;
+        return {a, b};
     }
 };
