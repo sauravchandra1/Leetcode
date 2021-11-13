dc1cd8ea9fa7e8aeada28f88eb11ad44a2799f6c
Saurav Chandra <sauravchandra125@gmail.com>
2021-11-13 04:13:39 +0000

    Sync LeetCode submission - Daily Temperatures (cpp)

--- problems/daily_temperatures/solution.cpp
+++ problems/daily_temperatures/solution.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& T) {
+    vector<int> dailyTemperatures(vector<int>& temp) {
         stack<int> stk;
-        int n = T.size();
-        vector<int> ans(n, 0);
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stk.empty() && T[stk.top()] <= T[i]) stk.pop();
-            if (!stk.empty()) {
-                ans[i] = stk.top() - i;
-            }
+        int len = temp.size();
+        vector<int> ans(len, 0);
+        stk.push(len - 1);
+        for (int i = len - 2; i >= 0; i--) {
+            while (!stk.empty() && temp[stk.top()] <= temp[i]) stk.pop();
+            if (!stk.empty()) ans[i] = stk.top() - i;
             stk.push(i);
         }
         return ans;
