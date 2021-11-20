a6bdb85a217e9a3814873a7ff1c072c749413a10
Saurav Chandra <sauravchandra125@gmail.com>
2021-11-20 04:41:02 +0000

    Sync LeetCode submission - Single Element in a Sorted Array (cpp)

--- problems/single_element_in_a_sorted_array/solution.cpp
+++ problems/single_element_in_a_sorted_array/solution.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int ans = 0;
-        for (auto v : nums) ans = ans ^ v;
-        return ans;
+        int len = nums.size();
+        int l = 0, r = len - 1, m;
+        while (l < r) {
+            m = (l + r) / 2;
+            if (m + 1 < len && nums[m] == nums[m + 1]) {
+                if ((len - m) % 2 == 0) r = m - 1;
+                else l = m + 2;
+            }
+            else if (m - 1 >= 0 && nums[m] == nums[m - 1]) {
+                if ((m + 1) % 2 == 0) l = m + 1;
+                else r = m - 2;
+            }
+            else return nums[m];
+        }
+        return nums[l];
     }
 };
