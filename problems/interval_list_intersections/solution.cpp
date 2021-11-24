258fad5c155cb13b6fd446f3da969288330b158e
Saurav Chandra <sauravchandra125@gmail.com>
2021-11-24 01:24:22 +0000

    Sync LeetCode submission - Interval List Intersections (cpp)

--- problems/interval_list_intersections/solution.cpp
+++ problems/interval_list_intersections/solution.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
-        vector<vector<int>> vec;
-        if (!A.size() || !B.size()) return vec;
-        int len_a = A.size();
-        int len_b = B.size();
-        int i = 0, j = 0;
-        int s, e;
-        vector<int> a, b;
-        while (i < len_a && j < len_b) {
-            a = A[i];
-            b = B[j];
-            s = max(a[0], b[0]);
-            e = min(a[1], b[1]);
-            if (s <= e) {
-                vec.push_back({s, e});
-            }
-            if (a[1] > b[1]) {
-                j++;
-            } else if (b[1] > a[1]) {
-                i++;
-            } else {
-                i++;
-                j++;
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        int n1 = firstList.size(), n2 = secondList.size();
+        int l1 = 0, l2 = 0;
+        vector<vector<int>> ans;
+        while (l1 < n1 && l2 < n2) {
+            int l = max(firstList[l1][0], secondList[l2][0]);
+            int r = min(firstList[l1][1], secondList[l2][1]);
+            if (l <= r) ans.push_back({l, r});
+            if (firstList[l1][1] < secondList[l2][1]) l1++;
+            else if (secondList[l2][1] < firstList[l1][1]) l2++;
+            else {
+                l1++, l2++;
             }
         }
-        return vec;
+        return ans;
     }
-    
 };
