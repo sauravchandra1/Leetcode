194d65f9b48f0ac5016e387dd8cf86cbd1c29297
Saurav Chandra <sauravchandra125@gmail.com>
2021-12-04 01:15:50 +0000

    Sync LeetCode submission - Stream of Characters (cpp)

--- problems/stream_of_characters/solution.cpp
+++ problems/stream_of_characters/solution.cpp
@@ -1,44 +1,60 @@
-class StreamChecker {
-    public:
-        struct TreeNode {
-            vector < TreeNode * > nodes;
-            bool isword;
-            TreeNode() {
-                nodes = vector < TreeNode * > (26, NULL);
-                isword = false;
-            }
-        };
+const int N = 26;
 
-    TreeNode * root;
-    string build;
-
-    void insert(vector < string > & words) {
-        for (string & s: words) {
-            reverse(s.begin(), s.end());
-            TreeNode * temp = root;
-            for (char c: s) {
-                if (!temp -> nodes[c - 'a']) temp -> nodes[c - 'a'] = new TreeNode();
-                temp = temp -> nodes[c - 'a'];
-            }
-            temp -> isword = true;
-        }
-    }
-
-    StreamChecker(vector < string > & words) {
-        root = new TreeNode();
-        build = "";
-        insert(words);
-    }
-
-    bool query(char letter) {
-        build += letter;
-        TreeNode * temp = root;
-        for (int i = build.size() - 1; i >= 0 && temp; i--) {
-            temp = temp -> nodes[build[i] - 'a'];
-            if (temp && temp -> isword) {
-                return true;
-            }
-        }
-        return false;
+class TrieNode {
+public:
+    TrieNode* next[N];    
+    bool isLast;
+    
+    TrieNode() {
+        for (int i = 0; i < N; i++) next[i] = 0;
+        isLast = false;
     }
 };
+
+class Trie {
+    TrieNode* root;
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+    void insert(string &str) {
+        TrieNode* curr = root;
+        char v;
+        for (int i = str.size() - 1; i >= 0; i--) {
+            v = str[i];
+            if (!curr->next[v - 'a']) curr->next[v - 'a'] = new TrieNode();
+            curr = curr->next[v - 'a'];
+        }
+        curr->isLast = true;
+    }
+    bool find(string& str) {
+        TrieNode* curr = root;
+        for (int i = str.size() - 1; i >= 0; i--) {
+            if (!curr->next[str[i] - 'a']) 
+                return false;
+            curr = curr->next[str[i] - 'a'];
+            if (curr->isLast) return true;
+        }
+        return curr->isLast;
+    }
+};
+
+class StreamChecker {
+public:
+    Trie T;
+    string str;
+    StreamChecker(vector<string>& words) {
+        for (auto& v : words) T.insert(v);
+    }
+    
+    bool query(char letter) {
+        str += letter;
+        return T.find(str);
+    }
+};
+
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * StreamChecker* obj = new StreamChecker(words);
+ * bool param_1 = obj->query(letter);
+ */
