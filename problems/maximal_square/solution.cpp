995f53aac59d6c2dde301004e0cc849d5a4c949d
Saurav Chandra <sauravchandra125@gmail.com>
2021-12-17 05:47:29 +0000

    Sync LeetCode submission - Maximal Square (cpp)

--- problems/maximal_square/solution.cpp
+++ problems/maximal_square/solution.cpp
@@ -1,20 +1,23 @@
 class Solution {
-    public:
-        int maximalSquare(vector < vector < char >> & matrix) {
-            int n = matrix.size();
-            if (n == 0) return 0;
-            int m = matrix[0].size();
-            if (m == 0) return 0;
-            vector < vector < int >> dp(n + 1, vector < int > (m + 1, 0));
-            int maxsq = 0;
-            for (int j = 1; j <= m; j++) {
-                for (int i = 1; i <= n; i++) {
-                    if (matrix[i - 1][j - 1] == '1') {
-                        dp[i][j] = min(min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
-                        maxsq = max(maxsq, dp[i][j]);
-                    }
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        //base case
+        if(matrix.size()==0) return 0;
+        //initializing our sentinal variables
+        int maxSqr = 0, rows = matrix.size() , column = matrix[0].size();
+        //dp matrix that we will be making 
+        vector<vector<int>> dp(rows+1,vector<int>(column+1,0));
+        
+        //Iterate over the matrix
+        for(int i=1;i<=rows;++i){
+            for(int j=1;j<=column;++j){
+                //we found the 1 in our binary matrix 
+                if(matrix[i-1][j-1]=='1'){
+                    dp[i][j] = min({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]})+ 1;
+                    maxSqr=max(maxSqr,dp[i][j]);
                 }
             }
-            return maxsq * maxsq;
         }
+        return maxSqr*maxSqr;
+    }
 };
