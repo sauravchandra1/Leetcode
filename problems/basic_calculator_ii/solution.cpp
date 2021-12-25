44069449915b05320e38bca9f7af4f29124a429c
Saurav Chandra <sauravchandra125@gmail.com>
2021-12-25 05:37:43 +0000

    Sync LeetCode submission - Basic Calculator II (cpp)

--- problems/basic_calculator_ii/solution.cpp
+++ problems/basic_calculator_ii/solution.cpp
@@ -1,66 +1,41 @@
+
 class Solution {
 public:
     int calculate(string s) {
-        stack<char> stk;
-        stack<int> num;
-        bool is_div = false, is_mul = false;
-        for (int i = 0; i < s.size(); i++) {
-            auto v = s[i];
-            if (v == ' ') continue;
-            if (v >= '0' && v <= '9') {
-                int val = 0;
-                while (i < s.size() && s[i] >= '0' && s[i] <= '9') {
-                    val = val * 10 + (s[i] - '0');
-                    i++;
+        int len = s.length();
+        if (len == 0) return 0;
+        stack<int> stack;
+        int currentNumber = 0;
+        char operation = '+';
+        for (int i = 0; i < len; i++) {
+            char currentChar = s[i];
+            if (isdigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
+            }
+            if (!isdigit(currentChar) && !iswspace(currentChar) || i == len - 1) {
+                if (operation == '-') {
+                    stack.push(-currentNumber);
+                } else if (operation == '+') {
+                    stack.push(currentNumber);
+                } else if (operation == '*') {
+                    int stackTop = stack.top();
+                    stack.pop();
+                    stack.push(stackTop * currentNumber);
+                } else if (operation == '/') {
+                    int stackTop = stack.top();
+                    stack.pop();
+                    stack.push(stackTop / currentNumber);
                 }
-                i--;
-                if (is_div || is_mul) {
-                    int b = val;
-                    int a = num.top();
-                    num.pop();
-                    if (is_div) {
-                        val = a / b;
-                        is_div = false;
-                    } else {
-                        val = a * b;
-                        is_mul = false;
-                    }
-                }
-                num.push(val);
-            } else if (v == '/') {
-                is_div = true;
-            } else if (v == '*') {
-                is_mul = true;
-            } else {
-                stk.push(v);
+                operation = currentChar;
+                currentNumber = 0;
             }
         }
-        int ans = 0;
-        deque<char> lit;
-        deque<int> val;
-        while (!stk.empty()) {
-            lit.push_front(stk.top());
-            stk.pop();
+        int result = 0;
+        while (stack.size() != 0) {
+            result += stack.top();
+            stack.pop();
         }
-        while (!num.empty()) {
-            val.push_front(num.top());
-            num.pop();
-        }
-        while (!lit.empty()) {
-            int a = val.front();
-            val.pop_front();
-            int b = val.front();
-            val.pop_front();
-            auto op = lit.front();
-            lit.pop_front();
-            if (op == '+') {
-                ans = a + b;
-            } else {
-                ans = a - b;
-            }
-            val.push_front(ans);
-        }
-        if (!val.empty()) return val.front();
-        return ans;
+        return result;
     }
 };
+
