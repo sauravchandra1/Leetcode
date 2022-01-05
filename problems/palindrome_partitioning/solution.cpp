7037c929188a16dbfebfd470befa1caccc80108e
Saurav Chandra <sauravchandra125@gmail.com>
2022-01-05 09:31:01 +0000

    Sync LeetCode submission - Palindrome Partitioning (cpp)

--- problems/palindrome_partitioning/solution.cpp
+++ problems/palindrome_partitioning/solution.cpp
@@ -2,44 +2,35 @@ class Solution {
 public:
     vector<vector<string>> partition(string s) {
         int len = s.size();
-        vector<vector<bool>> 
-            palindrome(len + 1, vector<bool> (len + 1, false));
-        
-        function<bool(string)> isPalin = [&](string str) {
-            int l = 0, r = str.size() - 1;
-            while (l < r) {
-                if (str[l] != str[r]) return false;
-                l++, r--;
-            }
-            return true;
+        vector<vector<vector<string>>> dp(len + 1);
+        vector<int> dp1(len, -1);
+        vector<vector<int>> palin(len, vector<int>(len, -1));
+        auto isPalin = [&](string a) {
+            string b = a;
+            reverse(a.begin(), a.end());
+            return (a == b);
         };
-        
-        string temp;
-        for (int i = 0; i < len; i++) {
-            temp = "";
-            for (int j = i; j < len; j++) {
-                temp += s[j];
-                palindrome[i][j] = isPalin(temp);
-            }
-        }
-        vector<vector<string>> ans;
-        vector<string> vec;
-        function<void(int)> dfs = [&](int idx) {
-            if (idx == len) {
-                ans.push_back(vec);
-                return;
-            }
+        function<vector<vector<string>>(int)> dfs = [&](int idx) {
+            if (idx == len) 
+                return dp[len];
+            auto& res = dp[idx];
+            if (dp1[idx] != -1) return res;
+            dp1[idx] = 0;
             string temp;
             for (int i = idx; i < len; i++) {
                 temp += s[i];
-                if (palindrome[idx][i]) {
-                    vec.push_back(temp);
-                    dfs(i + 1);
-                    vec.pop_back();
+                if (palin[idx][i] == -1) palin[idx][i] = isPalin(temp);
+                if (palin[idx][i]) {
+                    auto ss = dfs(i + 1);
+                    for (auto v : ss) {
+                        v.insert(v.begin(), temp);
+                        res.push_back(v);
+                    }
+                    if (i + 1 == len) res.push_back({temp});
                 }
             }
+            return res;
         };
-        dfs(0);
-        return ans;
+        return dfs(0);
     }
 };
