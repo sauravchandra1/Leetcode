a5bba3a026d0e2acaadb68eed70bbdc0b03f738a
Saurav Chandra <sauravchandra125@gmail.com>
2022-01-10 01:25:36 +0000

    Sync LeetCode submission - Add Binary (cpp)

--- problems/add_binary/solution.cpp
+++ problems/add_binary/solution.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    pair<int, int> get(char aa, char bb, int cc) {
-            int a1 = aa - '0', b1 = bb - '0';
-            int sum = (a1 + b1 + cc) % 2;
-            int carry = (a1 + b1 + cc) / 2;
-            pair<int, int> ans = {sum, carry};
-            return ans;
-    }
     string addBinary(string a, string b) {
-        reverse(a.begin(), a.end());
-        reverse(b.begin(), b.end());
-        const int N = 1e4 + 10;
-        while ((int)a.size() < N) a.push_back('0');
-        while ((int)b.size() < N) b.push_back('0');
-        string ans = "";
-        pair<int, int> gg;
-        int cc = 0;
-        for (int i = 0; i < N; i++) {
-            gg = get(a[i], b[i], cc);
-            ans.push_back(gg.first + '0');
-            cc = gg.second;
+        int i = a.size() - 1, j = b.size() - 1, c = 0, s;
+        string ans;
+        while (i >= 0 || j >= 0) {
+            s = c;
+            if (i >= 0) s += a[i] - '0';
+            if (j >= 0) s += b[j] - '0';
+            c = s / 2;
+            ans += ((s % 2) + '0'); 
+            i--, j--;
         }
-        while ((int)ans.size() > 1 && ans.back() == '0') ans.pop_back();
+        if (c) ans += '1';
         reverse(ans.begin(), ans.end());
         return ans;
     }
