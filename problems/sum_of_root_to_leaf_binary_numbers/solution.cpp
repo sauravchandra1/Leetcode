86a8baed1ffa5a79aa740637488140604111fc6a
Saurav Chandra <sauravchandra125@gmail.com>
2022-01-11 02:25:08 +0000

    Sync LeetCode submission - Sum of Root To Leaf Binary Numbers (cpp)

--- problems/sum_of_root_to_leaf_binary_numbers/solution.cpp
+++ problems/sum_of_root_to_leaf_binary_numbers/solution.cpp
@@ -1,24 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-    public:
-        vector < int > vec;
-    int sum = 0;
-    int sumRootToLeaf(TreeNode * root) {
-        calculate(root);
-        return sum;
-    }
-
-    void calculate(TreeNode * root) {
-        if (root == NULL)
-            return;
-        vec.push_back(root -> val);
-        sumRootToLeaf(root -> left);
-        if (root -> left == NULL && root -> right == NULL) {
-            int p = vec.size() - 1;
-            for (int i = vec.size() - 1; i >= 0; i--) {
-                sum = sum + (vec[i] * pow(2, p - i));
-            }
-        }
-        sumRootToLeaf(root -> right);
-        vec.pop_back();
+public:
+    int ans = 0;
+    int sumRootToLeaf(TreeNode* root, int num = 0) {
+        if (root == 0) return 0;
+        sumRootToLeaf(root->left, (num << 1) + root->val);
+        sumRootToLeaf(root->right, (num << 1) + root->val);
+        if (!root->left && !root->right) ans += (num << 1) + root->val;
+        return ans;
     }
 };
